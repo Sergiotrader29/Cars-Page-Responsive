7f10028f005e90dcddd7ddde4253238405402e4d
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-07-24 19:25:17 -0400

    Start the Componet Find & Card

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './components/navbar/hero/Hero';
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
