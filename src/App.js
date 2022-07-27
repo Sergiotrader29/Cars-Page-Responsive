5b5a7534446d0a0f3411c5a7ab04c9d6e7b1aea6
Sergio vasquez <103544569+Sergiotrader29@users.noreply.github.com>
2022-07-26 22:09:49 -0400

    component luxury

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Driver from './components/driver/Driver';
 import Find from './components/find/Find';
 import Hero from './components/hero/Hero';
+import Luxury from './components/Luxury/Luxury';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Find />
       <Driver />
+      <Luxury />
     </div>
   );
 }
